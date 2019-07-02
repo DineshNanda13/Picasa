78737bb41162d7ec450d34ee58ec6d619302b655
Dinesh Nanda <dannynanda76@gmail.com>
2019-07-02 11:06:36 -0400

    Initial commit

---
+++ app/src/main/java/com/meterstoinches/picasa/MainActivity.java
@@ -0,0 +1,33 @@
+package com.meterstoinches.picasa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu1,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.picasa){
+            Intent intent = new Intent(getApplicationContext(),Main2Activity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
